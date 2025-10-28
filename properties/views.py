5266e4396bfc7dea2b692f9877a0bd1c64e23902
Simon <simonbayu2211@exgmail.com>
2025-10-28 15:48:09 +0300

    Cache Property List View

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.cache import cache_page
+from .models import Property  # Make sure you have a Property model
+from django.http import JsonResponse
+
+# Cache the view for 15 minutes (60 sec * 15)
+@cache_page(60 * 15)
+def property_list(request):
+    properties = Property.objects.all().values(
+        'id', 'title', 'description', 'price', 'address'
+    )
+    return JsonResponse(list(properties), safe=False)
